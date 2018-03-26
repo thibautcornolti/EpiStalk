9d9eb282fb1102a6fb74d03c10f3aca5429cd9af
Thibaut Cornolti <thibaut@cornolti.eu>
2018-03-27 00:25:35 +0200

    Remove js file for ts

--- web/src/tasks.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var cron = require('cron');
-var intra = require('./intra');
-new cron.CronJob("0 0 * * *", function () {
-    intra.fillDb();
-}, null, true);
-module.exports = undefined;
-//# sourceMappingURL=tasks.js.map
