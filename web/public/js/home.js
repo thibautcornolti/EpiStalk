5e425a65ffcf0ddce4fb7664ef5bb8f9653313ab
Thibaut Cornolti <thibaut@cornolti.eu>
2018-03-27 16:56:56 +0200

    Add global user in home.js

--- web/public/js/home.js
+++ web/public/js/home.js
@@ -1,15 +1,28 @@
 
-/* Replace all fields with the "get-upper-user" attribute with the user name. */
-function setUserFields() {
-    $.get("/api/user", function (data) {
-        let name = data.user.email.split("@")[0].toUpperCase().replace(".", " ");
-        $(".get-upper-user").text(name);
-    }).fail(function (data, textStatus, xhr) {
-        $(".get-upper-user").text("NaN");
-    });
+let user;
+let users;
+
+$.get("/api/user", function (data) {
+    user = data.user;
+    $.get("/api/users", function (data) {
+        user = data.users;
+    })
+    main();
+})
+
+function main() {
+    setUserFields();
 }
 
-setUserFields();
+/* Replace all fields with the "get-upper-user" attribute with the user name. */
+function setUserFields() {
+    if (user) {
+        let name = user.email.split("@")[0].toUpperCase().replace(".", " ");
+        $(".get-upper-user").text(name);
+    } else
+        $(".get-upper-user").text("NaN");
+}
+
 function getAllUsers() {
     $.get("/users", function (data) {
         var arr = JSON.parse(JSON.stringify(data.users));
@@ -21,12 +34,12 @@ function getAllUsers() {
             let log = arr[i].current_week_log == undefined ? '?' : arr[i].current_week_log;
             let lastName = arr[i].email.split('@')[0].split('.')[1].toUpperCase();
             let firstName = arr[i].email.split('.')[0].toUpperCase();
-            let tab = '<tr class="center"><th class="center" scope="row">' +    leaderboard + '</th><td>' + 
-                                                                                firstName + '</td><td>' + 
-                                                                                lastName + '</td><td>' + 
-                                                                                log + '</td><td>' + 
-                                                                                credit + '</td><td>' + 
-                                                                                gpa + '</td></tr>'
+            let tab = '<tr class="center"><th class="center" scope="row">' + leaderboard + '</th><td>' +
+                firstName + '</td><td>' +
+                lastName + '</td><td>' +
+                log + '</td><td>' +
+                credit + '</td><td>' +
+                gpa + '</td></tr>'
             $("#tableID").find('tbody').append(tab);
         };
     }).fail(function (data, textStatus, xhr) {
