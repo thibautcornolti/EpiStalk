58092378e5a846c573fe13d9d0c93ed265f547fc
Luc Brulet <luc.brulet@epitech.eu>
2018-03-27 23:01:27 +0000

    starting setting.html

--- web/public/js/home.js
+++ web/public/js/home.js
@@ -33,17 +33,20 @@ function buildLeaderboard() {
     console.log(arr);
     for (var i = 0; i < arr.length; i++) {
         let leaderboard = i + 1;
-        credit = (user.credit == undefined) ? credit = arr[i].credit == undefined ? ' data-toggle="tooltip" title="Tu n\'as pas partagé tes crédits">NaN' : '>' + arr[i].credit : credit = arr[i].credit == undefined ? ' data-toggle="tooltip" title="L\'utilisateur n\'a pas partagé ses crédits">?' : '>' + arr[i].credit;
-        gpa = (user.gpa == undefined) ? gpa = arr[i].gpa == undefined ? ' data-toggle="tooltip" title="Tu n\'as pas partagé ton GPA">NaN' : '>' + arr[i].gpa : gpa = arr[i].gpa == undefined ? ' data-toggle="tooltip" title="L\'utilisateur n\'a pas partagé son GPA">?' : '>' + arr[i].gpa;
-        log = (user.log == undefined) ? log = arr[i].current_week_log == undefined ? ' data-toggle="tooltip" title="Tu n\'as pas partagé ton log">NaN' : '>' + arr[i].current_week_log : log = arr[i].current_week_log == undefined ? ' data-toggle="tooltip" title="L\'utilisateur n\'a pas partagé son log">?' : '>' + arr[i].current_week_log;
+        credit = (user.credit == undefined) ? credit = arr[i].credit == undefined ? ' data-toggle="tooltip" title="Tu n\'as pas partagé tes crédits">?' : '>' + arr[i].credit : credit = arr[i].credit == undefined ? ' data-toggle="tooltip" title="L\'utilisateur n\'a pas partagé ses crédits">?' : '>' + arr[i].credit;
+        gpa = (user.gpa == undefined) ? gpa = arr[i].gpa == undefined ? ' data-toggle="tooltip" title="Tu n\'as pas partagé ton GPA">?' : '>' + arr[i].gpa : gpa = arr[i].gpa == undefined ? ' data-toggle="tooltip" title="L\'utilisateur n\'a pas partagé son GPA">?' : '>' + arr[i].gpa;
+        log = (user.log == undefined) ? log = arr[i].current_week_log == undefined ? ' data-toggle="tooltip" title="Tu n\'as pas partagé ton log">?' : '>' + arr[i].current_week_log : log = arr[i].current_week_log == undefined ? ' data-toggle="tooltip" title="L\'utilisateur n\'a pas partagé son log">?' : '>' + arr[i].current_week_log;
         let lastName = arr[i].email.toUpperCase().split('@')[0].split('.')[1];
         let firstName = arr[i].email.split('.')[0].toUpperCase();
         let tab = '<tr class="center"><th class="center" scope="row">' + leaderboard + '</th><td>' +
-            firstName + '</td><td>' +
-            lastName + '</td><td' +
-            log + '</td><td' +
-            credit + '</td><td' +
-            gpa + '</td></tr>'
+        firstName + '</td><td>' +
+        lastName + '</td><td' +
+        log + '</td><td' +
+        credit + '</td><td' +
+        gpa + '</td></tr>'
         $("#tableID").find('tbody').append(tab);
     }
+    $(document).ready(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
 }
