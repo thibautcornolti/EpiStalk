ae10f58e8ef0f29e456ebb721585946b32d75d34
Thibaut Cornolti <thibaut@cornolti.eu>
2018-04-09 20:50:13 +0200

    Remove debug

--- src/tasks.ts
+++ src/tasks.ts
@@ -4,8 +4,6 @@ import { clearAccounts } from './account'
 import { isMaster } from '../vars'
 
 async function plan() {
-    clearAccounts();
-    intra.fillDb("thibaut.cornolti@epitech.eu");
     new cron.CronJob("0 */12 * * *", () => {
         if (isMaster)
             intra.fillDb();
